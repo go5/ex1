8e06d7bb3de3029ac4b808e1e485fc82f38a0a7d
Leemited <tecooni@naver.com>
2014-10-07 17:52:52 +0900

    Update Search_Customer.java

--- src/customer/Search_Customer.java
+++ src/customer/Search_Customer.java
@@ -33,18 +33,21 @@ public class DB1 {
 
 				while(rs.next()){
 					if (rs.getString(2).equals(name)){
-						System.out.println("고객번호\t고객명\t\t주소\t\t\t나이\t전화번호\t가족\t\t관계");
+						System.out.println("고객번호\t고객명\t\t주소\t\t\t나이\t전화번호\t가족(관계)");
 						System.out.print(rs.getInt(1)+"\t\t");
 						System.out.print(rs.getString(2)+"\t\t");
 						System.out.print(rs.getString(3)+"\t");
 						System.out.print(rs.getInt(4)+"\t");
 						System.out.print(rs.getInt(5)+"\t");
+
 						while(frs.next()){
 							if(rs.getInt(1)==frs.getInt(1)){
-								System.out.print(frs.getString(2)+"\t\t");
-								System.out.print(frs.getString(3));
-							}
+								System.out.print(frs.getString(2)+"(");
+								System.out.print(frs.getString(3)+")\t");
+							}else
+								break;
 						}
+
 						System.out.println("");
 					}
 					else {

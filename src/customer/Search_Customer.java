3b061d32664da7e8ec64b66c95821be149fe7dbc
Leemited <tecooni@naver.com>
2014-10-08 09:52:05 +0900

    Update Search_Customer.java

--- src/customer/Search_Customer.java
+++ src/customer/Search_Customer.java
@@ -20,40 +20,38 @@ public class Search_Customer {
 		try{
 			con = DriverManager.getConnection(url, "dvd","1111"); 
 			Scanner scan = new Scanner(System.in);
-			while(true){
-				System.out.println("고객조회창");
-				System.out.print("전화번호조회 : ");
-				String phone_num = scan.next();
+			System.out.println("고객조회창");
+			System.out.print("전화번호조회 : ");
+			String phone_num = scan.next();
 
-				sql = "SELECT * FROM member"; //WHERE phone_num LIKE '"+phone_num+"$'";
-				stmt = con.createStatement(); 
-				rs = stmt.executeQuery(sql);
-				fsql = "SELECT * FROM family";
-				fstmt = con.createStatement(); 
-				frs = fstmt.executeQuery(fsql);
+			sql = "SELECT * FROM member"; //WHERE phone_num LIKE '"+phone_num+"$'";
+			stmt = con.createStatement(); 
+			rs = stmt.executeQuery(sql);
+			fsql = "SELECT * FROM family";
+			fstmt = con.createStatement(); 
+			frs = fstmt.executeQuery(fsql);
 
-				while(rs.next()){
-					if (rs.getString(5).equals(phone_num)){
-						System.out.println("고객번호\t고객명\t\t주소\t\t나이\t전화번호\t가족(관계)");
-						System.out.print(rs.getInt(1)+"\t\t");
-						System.out.print(rs.getString(2)+"\t\t");
-						System.out.print(rs.getString(3)+"\t\t");
-						System.out.print(rs.getInt(4)+"\t");
-						System.out.print(rs.getString(5)+"\t");
+			while(rs.next()){
+				if (rs.getString(5).equals(phone_num)){
+					System.out.println("고객번호\t고객명\t\t주소\t\t나이\t전화번호\t가족(관계)");
+					System.out.print(rs.getInt(1)+"\t\t");
+					System.out.print(rs.getString(2)+"\t\t");
+					System.out.print(rs.getString(3)+"\t\t");
+					System.out.print(rs.getInt(4)+"\t");
+					System.out.print(rs.getString(5)+"\t");
 
-						while(frs.next()){
-							if(rs.getInt(1)==frs.getInt(1)){
-								System.out.print(frs.getString(2)+"(");
-								System.out.print(frs.getString(3)+")\t");
-							}else
-								break;
-						}
-
-						System.out.println("");
+					while(frs.next()){
+						if(rs.getInt(1)==frs.getInt(1)){
+							System.out.print(frs.getString(2)+"(");
+							System.out.print(frs.getString(3)+")\t");
+						}else
+							break;
 					}
-					else {
-						System.out.println("검색된 고객이 없습니다.");
-					}
+
+					System.out.println("");
+				}
+				else {
+					System.out.println("검색된 고객이 없습니다.");
 				}
 			}
 		}
@@ -68,6 +66,5 @@ public class Search_Customer {
 			if(con != null) try{con.close();}catch(Exception err){}
 		}
 		return;
-		
 	}
 }

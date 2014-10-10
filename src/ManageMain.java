a099578af20d78024b50eb245ad15bcd33055658
keispace <keispace.kyj@gmail.com>
2014-10-10 14:43:09 +0900

    최종.

--- src/ManageMain.java
+++ src/ManageMain.java
@@ -46,58 +46,58 @@ public class ManageMain {
 			menu=scan.nextInt();
 
 			switch (menu) {
-			case 1:
+			case 1:// dvd 대여
 				rd.rent_DVD();
 				System.out.println();
 				break;
-			case 2:
+			case 2://dvd 반납
 				rrd.return_dvd();
 				System.out.println();
 				break;
-			case 3:
+			case 3://dvd 대여 현황 조회
 				rs.rent_state();
 				System.out.println();
 				break;
-			case 4:
+			case 4:// dvd 대여 이력 조회/관리
 				sh.search_history();
 				System.out.println();
 				break;
-			case 5:
+			case 5://dvd 전체 목록
 				fsd.scdvd();
 				System.out.println();
 				break;
-			case 6:
+			case 6://dvd 상세 조회
 				sd.scdvd();
 				System.out.println();
 				break;
-			case 7:
+			case 7://회원 전체 목록
 				fsc.search_Customer();
 				System.out.println();
 				break;
-			case 8:
+			case 8://회원 정보 상세 조회
 				sc.search_Customer();
 				System.out.println();
 				break;
-			case 9:
+			case 9://dvd 입력
 				id.insert_dvd();
 				System.out.println();
 				break;
-			case 10:
+			case 10://회원 추가
 				ic.insert_Customer();
 				System.out.println();
 				break;
-			case 11:
+			case 11://기존 회원 가족멤버 추가
 				int input=0;
 				System.out.println("가족을 추가할 id를 입력하세요");
 				input=scan.nextInt();
 				iff.insert_family(input);
 				System.out.println();
 				break;
-			case 99:
+			case 99:// 종료
 				System.out.println("시스템을 종료합니다.");
 				System.exit(0);
 				break;
-			default : 
+			default : //입력 오류
 				System.out.println("다시 입력해 주십시요.");
 				System.out.println();
 				continue;

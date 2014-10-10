2d914ceecdf6cd755596224040caf3cd0a173274
keispace <keispace.kyj@gmail.com>
2014-10-10 14:28:28 +0900

    다중 검색 수정, 중복 에러 제거.

--- src/customer/Search_Customer.java
+++ src/customer/Search_Customer.java
@@ -11,81 +11,83 @@ public class Search_Customer {
 	public void search_Customer(){
 		String url = "jdbc:oracle:thin:@192.168.10.21:1521:orcl";
 		Connection con = null;
-		Statement stmt = null;
-		ResultSet rs = null;
-		String sql;
+		Statement sstmt, stmt = null;
+		ResultSet rrs, rs = null;
+		String ssql, sql;
 		Statement fstmt = null;
 		ResultSet frs = null;
 		String fsql;
-		
+
 		try{
 			Class.forName("oracle.jdbc.driver.OracleDriver");
-			con = DriverManager.getConnection(url, "dvd","1111"); 
+			con = DriverManager.getConnection(url, " dvd","1111"); 
 			Scanner scan = new Scanner(System.in);
 			System.out.println("고객조회창");
 			System.out.print("전화번호조회 : ");
 			String phone_num = scan.next();
 
-			sql = "SELECT * FROM member"; 
+			sql = "SELECT * FROM member WHERE phone_num ="+phone_num; 
 			stmt = con.createStatement(); 
 			rs = stmt.executeQuery(sql);
-			
-			fsql = "SELECT * FROM family";
-			fstmt = con.createStatement(); 
-			frs = fstmt.executeQuery(fsql);
-			
-			
+
 
 			while(rs.next()){
-				if (rs.getString(5).equals(phone_num)){
-					System.out.println("고객번호\t고객명\t\t주소\t\t나이\t전화번호\t가족(관계)");
-					int id = rs.getInt(1);
-					System.out.print(rs.getInt(1)+"\t\t");
-					System.out.print(rs.getString(2)+"\t\t");
-					System.out.print(rs.getString(3)+"\t\t");
-					System.out.print(rs.getInt(4)+"\t");
-					System.out.print(rs.getString(5)+"\t");
+				//System.out.println(sql);
+				
+				//고객정보 출력
+				System.out.println("고객번호\t고객명\t\t주소\t\t나이\t전화번호\t가족(관계)");
+				int id = rs.getInt(1);
+				System.out.print(rs.getInt(1)+"\t\t");
+				System.out.print(rs.getString(2)+"\t\t");
+				System.out.print(rs.getString(3)+"\t\t");
+				System.out.print(rs.getInt(4)+"\t");
+				System.out.print(rs.getString(5)+"\t");
 
-					while(frs.next()){
-						if(rs.getInt(1)==frs.getInt(1)){
-							System.out.print(frs.getString(2)+"(");
-							System.out.print(frs.getString(3)+")\t");
-						}else
-							break;
-					}
+				fsql = "SELECT * FROM family WHERE cus_id="+id;
+				fstmt = con.createStatement(); 
+				frs = fstmt.executeQuery(fsql);
+				
+				ssql = "SELECT * FROM managerdvd WHERE cus_id = "+id; 
+				sstmt = con.createStatement(); 
+				rrs = sstmt.executeQuery(ssql);
 
+				//가족 멤버 출력
+				while(frs.next()){
+					System.out.print(frs.getString(2)+"(");
+					System.out.print(frs.getString(3)+")\t");
+				}
+				System.out.println();
+
+				//대여 이력 출력
+				System.out.println("대여 이력");
+				System.out.println("비디오 타이틀 \t\t 대여 일자 \t\t반납일자");
+				while(rrs.next()){
+					System.out.print(rrs.getString("title")+"\t\t");
+					System.out.print(rrs.getString("rent_date")+"\t\t");
+					System.out.print(rrs.getString("return_date"));
 					System.out.println();
-					//대여 이력 떠야 함. 
-					sql = "SELECT * FROM managerdvd WHERE cus_id = "+id; 
-					stmt = con.createStatement(); 
-					rs = stmt.executeQuery(sql);
-					System.out.println("대여 이력");
-					System.out.println("비디오 타이틀 \t\t 대여 일자 \t\t반납일자");
-					while(rs.next()){
-						System.out.print(rs.getString("title")+"\t\t");
-						System.out.print(rs.getString("rent_date")+"\t\t");
-						System.out.print(rs.getString("return_date"));
-						System.out.println();
-					}
-					return;
-					
 				}
-				else {
-					System.out.println("검색된 고객이 없습니다.");
-					return; //회원 수만큼 에러나는 거니까 이 부분만 추가하면 될 듯.? 테스트 필요.
-				}
+		//		System.out.println("검색 종료");
+				//break;
+
+
 			}
-		}
-		catch(Exception err){
-			err.printStackTrace();
-		}
-		finally{
-			if(frs != null) try{frs.close();}catch(Exception err){}
-			if(rs != null) try{rs.close();}catch(Exception err){}
-			if(fstmt != null) try{fstmt.close();}catch(Exception err){}
-			if(stmt != null) try{stmt.close();}catch(Exception err){}
-			if(con != null) try{con.close();}catch(Exception err){}
-		}
-		return;
+		
+
+
+
 	}
+	catch(Exception err){
+		err.printStackTrace();
+	}
+	finally{
+		System.out.println("-------------------------------------------");
+		if(frs != null) try{frs.close();}catch(Exception err){}
+		if(rs != null) try{rs.close();}catch(Exception err){}
+		if(fstmt != null) try{fstmt.close();}catch(Exception err){}
+		if(stmt != null) try{stmt.close();}catch(Exception err){}
+		if(con != null) try{con.close();}catch(Exception err){}
+	}
+	return;
+}
 }
